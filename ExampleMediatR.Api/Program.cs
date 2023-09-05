d5de433fc6ad65890fd0c533d1ff61dea32685eb
Paul Frank <pawelspam42@gmail.com>
2023-09-05 02:06:00 +0200

    Lifetime of services changed

--- ExampleMediatR.Api/Program.cs
+++ ExampleMediatR.Api/Program.cs
@@ -11,12 +11,24 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<ICustomersRepository, CustomersRepository>();
-builder.Services.AddSingleton<IOrdersRepository, OrdersRepository>();
-builder.Services.AddSingleton<IMapper, Mapper>();
-builder.Services.AddMediatR(typeof(Program)); // <- auto scan for handlers and register them in DI.
+builder.Services.AddScoped<ICustomersRepository, CustomersRepository>();
+builder.Services.AddScoped<IOrdersRepository, OrdersRepository>();
+
+//builder.Services.AddSingleton<IMapper, Mapper>();
+//builder.Services.AddMediatR(cfg=>cfg.RegisterServicesFromAssemblies(typeof(GetMakesQueryHandler).GetTypeInfo().Assembly));
+//builder.Services.AddMediatR(typeof(Program)); // <- auto scan for handlers and register them in DI.
 builder.Services.AddDbContext<ShopDbContext>(options => 
         options.UseSqlServer(builder.Configuration.GetConnectionString("ShopDb")));
+
+//builder.Services.AddDbContext<ShopDbContext>(o =>
+//{
+//    var connectionString = builder.Configuration.GetConnectionString("ShopDb");
+//    o.UseSqlServer(connectionString, s =>
+//    {
+//        s.CommandTimeout(180);
+//        s.MigrationsAssembly(Assembly.GetExecutingAssembly().ToString());
+//    });
+//});
 
 var app = builder.Build();
 
